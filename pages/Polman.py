81b15d44ca356f247821a825d82baeb00de284f6
iamndia <iamndiaa@gmail.com>
2024-11-14 14:16:12 +0800

    update code

--- pages/Polman.py
+++ pages/Polman.py
@@ -25,6 +25,7 @@ for i in range(1, 6):
     clusters.append(kmeans.inertia_)
 
 # Plot Elbow
+st.subheader("Elbow Method")
 fig1, ax1 = plt.subplots(figsize=(12, 8))
 sns.lineplot(x=list(range(1, 6)), y=clusters, ax=ax1)
 ax1.set_title("Mencari Elbow")
@@ -51,6 +52,7 @@ df['Cluster'] = df['Cluster'].map(cluster_mapping)
 cluster_colors = {'Rendah': 'green', 'Sedang': 'blue', 'Tinggi': 'red'}
 
 # Membuat plot scatter
+st.subheader("Plot Clustering")
 fig2, ax2 = plt.subplots(figsize=(15, 6))
 sns.scatterplot(x='Kecamatan', y='Produktivitas(Kg/Ha)', hue='Cluster', palette=cluster_colors, s=150, data=df, ax=ax2)
 ax2.set_title("Hasil Clustering")
@@ -62,12 +64,30 @@ st.header("Hasil Cluster")
 st.write(df)
 
 # Menampilkan Peta dengan hasil pengelompokan
-# st.subheader("Peta Hasil Pengelompokan")
-# map_loc = folium.Map(location=[df['latitude'].mean(), df['longtitude'].mean()], zoom_start=8)
-# for idx, row in df.iterrows():
-#     folium.Marker(
-#         location=[row['latitude'], row['longtitude']], 
-#         tooltip=f"Kecamatan {row['Kabupaten']} (Cluster: {row['Cluster']})",
-#         icon=folium.Icon(color='green' if row['Cluster'] == 'Rendah' else 'blue' if row['Cluster'] == 'Sedang' else 'red')
-#     ).add_to(map_loc)
-# st_folium(map_loc, width=700)
+st.subheader("Peta Hasil Pengelompokan")
+map_loc = folium.Map(location=[df['latitude'].mean(), df['longtitude'].mean()], zoom_start=8)
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row['latitude'], row['longtitude']], 
+        tooltip=f"Kecamatan {row['Kecamatan']} (Cluster: {row['Cluster']})",
+        icon=folium.Icon(color='green' if row['Cluster'] == 'Rendah' else 'blue' if row['Cluster'] == 'Sedang' else 'red')
+    ).add_to(map_loc)
+st_folium(map_loc, width=600)
+
+# Custom CSS untuk mengubah warna latar belakang
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #020249FF;
+    }
+    .stApp {
+        background-color: #02023EFF;
+    }
+    [data-testid="stSidebarContent"] {
+    background-color: #020249B9;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
--- pages/majene.py
+++ pages/majene.py
@@ -26,6 +26,7 @@ for i in range(1, 6):
     clusters.append(kmeans.inertia_)
 
 # Plot Elbow
+st.subheader("Elbow Method")
 fig1, ax1 = plt.subplots(figsize=(12, 8))
 sns.lineplot(x=list(range(1, 6)), y=clusters, ax=ax1)
 ax1.set_title("Mencari Elbow")
@@ -52,6 +53,7 @@ df['Cluster'] = df['Cluster'].map(cluster_mapping)
 cluster_colors = {'Rendah': 'green', 'Sedang': 'blue', 'Tinggi': 'red'}
 
 # Membuat plot scatter
+st.subheader("Plot Clustering")
 fig2, ax2 = plt.subplots(figsize=(12, 6))
 sns.scatterplot(x='Kabupaten', y='PRODUKTIVITAS (Kg/Ha/Thn)', hue='Cluster', palette=cluster_colors, s=150, data=df, ax=ax2)
 ax2.set_title("Hasil Clustering")
@@ -63,12 +65,30 @@ st.header("Hasil Cluster")
 st.write(df)
 
 # Menampilkan Peta dengan hasil pengelompokan
-# st.subheader("Peta Hasil Pengelompokan")
-# map_loc = folium.Map(location=[df['latitude'].mean(), df['longtitude'].mean()], zoom_start=8)
-# for idx, row in df.iterrows():
-#     folium.Marker(
-#         location=[row['latitude'], row['longtitude']], 
-#         tooltip=f"Kecamatan {row['Kabupaten']} (Cluster: {row['Cluster']})",
-#         icon=folium.Icon(color='green' if row['Cluster'] == 'Rendah' else 'blue' if row['Cluster'] == 'Sedang' else 'red')
-#     ).add_to(map_loc)
-# st_folium(map_loc, width=700)
+st.subheader("Peta Hasil Pengelompokan")
+map_loc = folium.Map(location=[df['latitude'].mean(), df['longtitude'].mean()], zoom_start=8)
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row['latitude'], row['longtitude']], 
+        tooltip=f"Kecamatan {row['Kabupaten']} (Cluster: {row['Cluster']})",
+        icon=folium.Icon(color='green' if row['Cluster'] == 'Rendah' else 'blue' if row['Cluster'] == 'Sedang' else 'red')
+    ).add_to(map_loc)
+st_folium(map_loc, width=700)
+
+# Custom CSS untuk mengubah warna latar belakang
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #020249FF;
+    }
+    .stApp {
+        background-color: #02023EFF;
+    }
+    [data-testid="stSidebarContent"] {
+    background-color: #020249B9;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
